24b5cad42f73c2cff748287c1377207f0260a9f8
Andrei Tsakunou <andrey.tsakunov@gmail.com>
2022-08-01 23:35:55 +0300

    fix: fix db connection issue '

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,10 @@
 import { Module } from '@nestjs/common';
-import { PostgressService } from './services/pg.sequelize.service';
 
 import { UsersModule } from './users/users.module';
 
 @Module({
   imports: [UsersModule],
   controllers: [],
-  providers: [PostgressService],
+  providers: [],
 })
 export class AppModule {}
--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -3,9 +3,15 @@ import { InMemoryUsersRepository } from 'src/data-access/in-memory-users.reposit
 import { Module } from '@nestjs/common';
 import { UsersController } from './controllers/users.controller';
 import { UsersService } from './services/users.service';
+import { PostgressService } from 'src/services/pg.sequelize.service';
 
 @Module({
   controllers: [UsersController],
-  providers: [UsersService, InMemoryUsersRepository, SequelizeUsersRepository],
+  providers: [
+    UsersService,
+    InMemoryUsersRepository,
+    SequelizeUsersRepository,
+    PostgressService,
+  ],
 })
 export class UsersModule {}
